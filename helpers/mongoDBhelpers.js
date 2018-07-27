5994cc7c19824640afb3ca340fd5077b73704db3
ArkejGit <robert.krawczyk92@gmail.com>
2018-07-27 14:21:51 +0200

    Add 'catch' for handling promise rejection

--- helpers/mongoDBhelpers.js
+++ helpers/mongoDBhelpers.js
@@ -3,13 +3,16 @@
 const mongoose = require('mongoose');
 const db = require('../config/database');
 
+const { handleError } = require('../helpers/errorHandling');
+
 module.exports = {
 
   clearCollectionDB: (collection) => {
     mongoose.connection.db.listCollections({ name: collection })
       .next((e, collinfo) => {
         if (collinfo) {
-          mongoose.connection.collections[collinfo.name].drop();
+          mongoose.connection.collections[collinfo.name].drop()
+            .catch(err => handleError(err));
         }
       });
   },
