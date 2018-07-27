3def4b03104af137bc24be06cab768e2d0b1bbd8
ArkejGit <robert.krawczyk92@gmail.com>
2018-07-27 16:41:42 +0200

    Add production database

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
 'use strict';
 
-if (process.env.NODE_ENV === 'test') {
+if (process.env.NODE_ENV === 'production') {
+  module.exports = { mongoURL: 'mongodb://netmovie:netmovie1@ds257551.mlab.com:57551/netmovie-api' };
+} else if (process.env.NODE_ENV === 'test') {
   module.exports = { mongoURL: 'mongodb://localhost:27017/netmovie-test' };
 } else {
   module.exports = { mongoURL: 'mongodb://localhost:27017/netmovie' };
